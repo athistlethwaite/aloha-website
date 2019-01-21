05f4129fc4ee66588c82123abcec8c722fdb3789
Alysha Thistlethwaite <alysha.thistlethwaite@gmail.com>
2019-01-21 15:28:01 -0800

    Project 1 Submission

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
-# Aloha project
-This is a project to showcase HTML and CSS to build a reponsive website as part of the Web Development Program at RED Academy. 
+# Aloha Project
 
-## Skills learned 
+This project showcases the HTML, CSS & JavaScript skills I learned in the first two weeks as part of the Web Development Program at RED Academy. It also shows how to build a reponsive website, and the importance of creating a mobile first design.
+
+## Skills Learned:
+
 - HTML
 - CSS
-- Responsive Web Design 
-- Git 
+- Flexboxes
+- Responsive Web Design
+- Git
 - Command Line Interface
+- JavaScript
+- jQuery
+- & Git
---
+++ index.js
@@ -0,0 +1,55 @@
+$(function() {
+  // Smooth Scrolling Source: https://css-tricks.com/smooth-scrolling-accessibility/
+
+  $('.main-navigation a[href*="#"]:not([href="#"])').click(function() {
+    if (
+      location.pathname.replace(/^\//, "") ==
+        this.pathname.replace(/^\//, "") &&
+      location.hostname == this.hostname
+    ) {
+      var target = $(this.hash);
+      target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+      if (target.length) {
+        $("html, body").animate(
+          {
+            scrollTop: target.offset().top
+          },
+          1000
+        );
+        return false;
+      }
+    }
+  });
+
+  $(".main-carousel").flickity({
+    cellAlign: "left",
+    contain: true,
+    percentPosition: false,
+    imagedLoaded: true,
+    autoPlay: true,
+    speed: 400,
+    prevNextButtons: false
+  });
+});
+
+$(function() {
+  $(".sign-up-form button").click(function(event) {
+    event.preventDefault();
+    if ($("#your-email").val() == "") {
+      alert("Please enter a valid e-mail address.");
+    } else {
+      alert("Thank you for subscribing!");
+    }
+  });
+});
+
+$(function() {
+  var cartItems = 0;
+  $(".featured").on("click", "button", function(event) {
+    event.preventDefault();
+    cartItems++;
+    $(".cart-items")
+      .text(cartItems)
+      .show();
+  });
+});
